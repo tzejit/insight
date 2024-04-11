13c4f290350a066360788c994f96897219aaba6e
SimJunYou <simjunyou99@gmail.com>
2024-04-12 01:57:22 +0800

    Added more helper functions for interfacing with data and jobs

---
+++ src/hooks/dataManagement.js
@@ -0,0 +1,75 @@
+import { uploadData, downloadData, getUrl } from "aws-amplify/storage";
+
+async function upload_file(user_id, file_id, file) {
+    // file_id can come with or without .csv suffix (doesn't matter)
+    // TODO: Verify that file uploaded is a CSV
+    console.debug("Starting upload of file", file_id);
+
+    try {
+        const uploadTask = uploadData({
+            key: `userdata/${user_id}/${file_id}`, // key NEEDS to be in this format - userId folder then fileId file
+            data: file,
+            options: {
+                accessLevel: "private", // authorized users only!
+                // onProgress, // Optional progress callback.
+            },
+        });
+        uploadTask.result.then(
+            (value) => console.info("Upload success", value),
+            (reason) => console.error("Error uploading file", reason)
+        );
+    } catch (error) {
+        console.error("Error uploading file", error);
+    }
+}
+
+async function get_result(user_id, job_id) {
+    // Retrieves contents of results
+    // This function assumes that the job is 'COMPLETED'
+    console.debug("Retrieving results of job", job_id);
+
+    try {
+        // Downloads file content to memory
+        // The file name is always ${job_id}.json
+        const { body } = await downloadData({
+            key: `results/${user_id}/${job_id}.json`,
+            options: {
+                accessLevel: "private", // access level of the file being downloaded
+                // TODO: (low) Download progress?
+                // onProgress: (event) => {
+                //     console.log(event.transferredBytes);
+                // }, // optional progress callback
+            },
+        }).result;
+
+        const result_json = await body.text();
+        console.info("Download success", result_json);
+        return result_json;
+    } catch (error) {
+        console.error("Error downloading file", error);
+    }
+}
+
+async function get_result_url(user_id, job_id) {
+    // Retrieves download URL of results as a string
+    // This function assumes that the job is 'COMPLETED'
+    console.debug("Retrieving results of job", job_id);
+
+    try {
+        const url_result = await getUrl({
+            key: `results/${user_id}/${job_id}.json`,
+            options: {
+                accessLevel: "private", // access level of the file being downloaded
+                expiresIn: 60, // 60 seconds for now
+            },
+        });
+
+        console.info("Download URL retrieved", url_result.url.href);
+        console.info("Expires at", url_result.expiresAt);
+        return url_result.url.href;
+    } catch (error) {
+        console.error("Error downloading file", error);
+    }
+}
+
+export { upload_file, get_result, get_result_url };
---
+++ src/hooks/jobManagement.js
@@ -0,0 +1,139 @@
+import { generateClient } from "aws-amplify/api";
+import { createJob } from "../graphql/mutations";
+import { getJob, listJobs } from "../graphql/queries";
+import { onUpdateJob } from "../graphql/subscriptions";
+
+async function submit_job(userId, fileId) {
+    if (!userId || !fileId) {
+        console.error(
+            "Cannot submit job without userId or fileId!",
+            userId,
+            fileId
+        );
+        return;
+    }
+    const client = generateClient();
+    const newJob = {
+        // for graphql in general, refer to src/graphql/schema.graphql for more details
+        file_id: fileId,
+        user_id: userId,
+
+        // job_status is actually an enum field, but we just use string so long as it matches enum name
+        job_status: "WAITING",
+
+        // TODO: (high priority) add input fields for everything below here:
+        job_name: "test_job_name",
+        job_config: {
+            prompt_id: "prompt.txt",
+            product_name: "Amazon Kindle",
+            review_title_col: "reviews.title",
+            review_text_col: "reviews.text",
+            review_rating_col: "reviews.rating",
+        },
+    };
+
+    console.info("Submitting new job", fileId);
+    try {
+        const graphql_res = await client.graphql({
+            query: createJob,
+            variables: {
+                input: newJob,
+            },
+        });
+        const new_job_id = graphql_res.data.createJob.id;
+        console.info("Successfully submitted new job with id:", new_job_id);
+
+        return new_job_id;
+    } catch (error) {
+        console.error("Error submitting new job", error);
+        console.error("Job details", newJob);
+        throw error; // trigger rejection in promise
+    }
+}
+
+async function start_job_polling(
+    jobId,
+    delay_in_seconds,
+    job_progress_callback
+) {
+    console.info("Polling job status for", jobId);
+    const sleep = (seconds) =>
+        new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+
+    let new_job_status = (await get_job(jobId)).job_status;
+    let old_job_status = new_job_status;
+    while (new_job_status !== "COMPLETED" && new_job_status !== "FAILED") {
+        await sleep(delay_in_seconds); // polling with 5 second gap
+        new_job_status = (await get_job(jobId)).job_status;
+        console.log("Poll result:", new_job_status);
+        if (new_job_status !== old_job_status) {
+            job_progress_callback(new_job_status);
+            old_job_status = new_job_status;
+        }
+    }
+    console.debug("Polling completed for", jobId);
+}
+
+async function get_job(jobId) {
+    const client = generateClient();
+    try {
+        const result = await client.graphql({
+            query: getJob,
+            variables: { id: jobId },
+        });
+        console.info("Successfully retrieved job with id", jobId);
+        const this_job = result.data.getJob;
+        return this_job; // a Job object (as defined in the graphql file)
+    } catch (error) {
+        console.error("Error getting job", error);
+        return;
+    }
+}
+
+async function list_jobs(userId) {
+    const client = generateClient();
+    const filter_params = {
+        filter: {
+            user_id: {
+                eq: userId, // filter only matching user IDs
+            },
+        },
+    };
+    try {
+        const result = await client.graphql({
+            query: listJobs,
+            variables: { filter_params },
+        });
+        console.info("Successfully listed jobs");
+        const job_list = result.data.listJobs.items;
+        return job_list;
+    } catch (error) {
+        console.error("Error getting list of jobs", error);
+        return;
+    }
+}
+
+async function get_latest_job(userId) {
+    const client = generateClient();
+    const filter_params = {
+        filter: {
+            user_id: {
+                eq: userId, // filter only matching user IDs
+            },
+        },
+    };
+    try {
+        const result = await client.graphql({
+            query: listJobs,
+            variables: filter_params,
+        });
+        console.info("Successfully listed jobs");
+        const job_list = result.data.listJobs.items;
+        return job_list;
+    } catch (error) {
+        console.error("Error getting list of jobs", error);
+        return;
+    }
+}
+
+export { submit_job, start_job_polling, get_job, list_jobs };
