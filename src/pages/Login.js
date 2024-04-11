d2938fe0a21b494cb30f2af767639ec77595de0f
SimJunYou <simjunyou99@gmail.com>
2024-04-12 01:56:02 +0800

    Updated Login to use new auth and handle auth edge cases

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,55 +1,108 @@
-import { ThemeProvider } from '@mui/material/styles';
-import { Typography } from '@mui/material';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Alert from '@mui/material/Alert';
-import InsightTitle from '../components/typography/InsightTitle';
-import BlackButton from '../components/buttons/BlackButton';
-import TextFieldYellow from '../components/inputs/TextFieldYellow';
-import theme from '../components/themes/MainTheme';
-import authenticate from '../hooks/authenticate';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { ThemeProvider } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+
+import { signIn } from "aws-amplify/auth";
+
+import InsightTitle from "../components/typography/InsightTitle";
+import BlackButton from "../components/buttons/BlackButton";
+import TextFieldYellow from "../components/inputs/TextFieldYellow";
+import theme from "../components/themes/MainTheme";
+import { fetch_user_auth_status, do_sign_out } from "../hooks/auth";
 
 function Login() {
     const navigate = useNavigate();
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
     async function loginApiCall() {
         try {
-            await authenticate(username, password)
-            setUsername('')
-            setPassword('')
-            navigate('/welcome')
-        } catch(error) {
-            console.log(error)
-            setError(error.message)
+            console.info("Attempting login");
+            const { isSignedIn, nextStep } = await signIn({
+                username,
+                password,
+            });
+            switch (nextStep.signInStep) {
+                case "DONE":
+                    console.info(`${username} successfully signed in!`);
+                    break;
+                case "CONFIRM_SIGN_UP":
+                    console.warn(
+                        `${username} tried to sign in but hasn't verified OTP yet`
+                    );
+                    setError("You have not verified your account yet.");
+                    break;
+                default:
+                    console.warn(`${username} failed to sign in`);
+            }
+            setUsername("");
+            setPassword("");
+            navigate("/welcome");
+        } catch (error) {
+            if (error.name === "UserAlreadyAuthenticatedException") {
+                console.warn(
+                    "There is already a user logged in - logging them out now"
+                );
+                const user_auth_status = await fetch_user_auth_status();
+                await do_sign_out();
+                loginApiCall(); // try login again after previous user is logged out
+            } else {
+                console.error("Error signing in", error);
+                setError(error.message);
+            }
         }
     }
 
     return (
         <ThemeProvider theme={theme}>
-            <Box sx={{ backgroundColor: 'yellow.main', flexDirection: 'column' }} height='100vh' width='100vw' display="flex" justifyContent="space-evenly" alignItems="center">
+            <Box
+                sx={{ backgroundColor: "yellow.main", flexDirection: "column" }}
+                height="100vh"
+                width="100vw"
+                display="flex"
+                justifyContent="space-evenly"
+                alignItems="center"
+            >
                 <InsightTitle />
                 <Stack justifyContent="center" alignItems="center" spacing={2}>
                     <Box width="100%">
-                        <Typography color='black' variant='body1' fontWeight='fontWeightMedium' align="left">Username</Typography>
+                        <Typography
+                            color="black"
+                            variant="body1"
+                            fontWeight="fontWeightMedium"
+                            align="left"
+                        >
+                            Username
+                        </Typography>
                     </Box>
-                    <TextFieldYellow onChange={(e) => setUsername(e.target.value)} />
+                    <TextFieldYellow
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                     <Box width="100%">
-                        <Typography color='black' variant='body1' fontWeight='fontWeightMedium' align="left">Password</Typography>
+                        <Typography
+                            color="black"
+                            variant="body1"
+                            fontWeight="fontWeightMedium"
+                            align="left"
+                        >
+                            Password
+                        </Typography>
                     </Box>
-                    <TextFieldYellow onChange={(e) => setPassword(e.target.value)} type="password"/>
-                    {error ? <Alert severity="error">{error}</Alert> : ''}
+                    <TextFieldYellow
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                    />
+                    {error ? <Alert severity="error">{error}</Alert> : ""}
                 </Stack>
-                <BlackButton onClick={loginApiCall}>
-                    Login
-                </BlackButton>
+                <BlackButton onClick={loginApiCall}>Login</BlackButton>
             </Box>
         </ThemeProvider>
-
     );
 }
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,97 +1,199 @@
-import { ThemeProvider } from '@mui/material/styles';
-import { Box, Typography } from '@mui/material';
-import Stack from '@mui/material/Stack';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import Alert from '@mui/material/Alert';
-import InsightTitle from '../components/typography/InsightTitle';
-import TextFieldYellow from '../components/inputs/TextFieldYellow';
-import BlackButton from '../components/buttons/BlackButton';
-import theme from '../components/themes/MainTheme';
-import { CognitoUser, CognitoUserAttribute } from 'amazon-cognito-identity-js';
-import userpool from '../components/userpool';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
+import { ThemeProvider } from "@mui/material/styles";
+import { Box, Typography } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Alert from "@mui/material/Alert";
+
+import { signUp, confirmSignUp } from "aws-amplify/auth";
+
+import InsightTitle from "../components/typography/InsightTitle";
+import TextFieldYellow from "../components/inputs/TextFieldYellow";
+import BlackButton from "../components/buttons/BlackButton";
+import theme from "../components/themes/MainTheme";
 
 function Signup() {
     const navigate = useNavigate();
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [email, setEmail] = useState('')
-    const [otp, setOtp] = useState('')
-    const [error, setError] = useState('')
-    const [showOtp, setShowOtp] = useState(false)
+    const [username, setUsername] = useState(""); // username refers to email now, we no longer have conventional usernames
+    const [password, setPassword] = useState("");
+    // const [email, setEmail] = useState("");
+    const [isSignupSuccess, setSignupSuccess] = useState("");
+    const [confirmationCode, setConfirmationCode] = useState(""); // renamed from otp for convenience
+    const [error, setError] = useState("");
+    const [showOTPVerification, setShowOTPVerification] = useState(false);
 
+    async function handleSignUpConfirmation() {
+        try {
+            const { isSignUpComplete, nextStep } = await confirmSignUp({
+                username,
+                confirmationCode,
+            });
+            setSignupSuccess(isSignUpComplete);
 
-    function signupApiCall() {
-        const attributeList = []
-        attributeList.push(
-            new CognitoUserAttribute({
-                Name: 'email',
-                Value: email,
-            })
-        )
-        userpool.signUp(username, password, attributeList, null, (err, data) => {
-            if (err) {
-                setError(err.message)
+            if (isSignUpComplete) {
+                navigate("/login");
             } else {
-                setShowOtp(true)
-                setError('')
+                console.warn("Wrong OTP!");
+                setError("Wrong OTP. Please try again.");
             }
-        });
+        } catch (error) {
+            console.error("Error confirming sign up", error);
+            setError(error.message);
+        }
     }
 
-    function otpApiCall() {
-        const user = new CognitoUser({
-            Username: username,
-            Pool: userpool,
-        });
-        user.confirmRegistration(otp, true, (err, data) => {
-            if (err) {
-                setError(err.message)
-            } else {
-                navigate('/login')
+    // From https://docs.amplify.aws/javascript/build-a-backend/auth/enable-sign-up/
+    async function handleSignUp() {
+        try {
+            const { isSignUpComplete, userId, nextStep } = await signUp({
+                username,
+                password,
+                options: {
+                    // optional
+                    autoSignIn: true, // or SignInOptions e.g { authFlowType: "USER_SRP_AUTH" }
+                },
+            });
+            // nextStep.signUpStep has values ("DONE" | "CONFIRM_SIGN_UP" | "COMPLETE_AUTO_SIGN_IN")
+            switch (nextStep.signUpStep) {
+                case "CONFIRM_SIGN_UP":
+                    setShowOTPVerification(true);
+                    console.info(`${userId} tried sign-up, awaiting OTP`);
+                    break;
+                case "DONE":
+                    setShowOTPVerification(false);
+                    console.info(`Signed up ${userId} successfully!`);
+                    break;
+                default:
+                    setShowOTPVerification(false);
+                    console.warn(`${userId} failed to sign up!`);
             }
-        });
+        } catch (error) {
+            console.error("Error signing up", error);
+            setError(error.message);
+        }
     }
+
     return (
         <ThemeProvider theme={theme}>
-            {showOtp ?
-                <Box sx={{ backgroundColor: 'yellow.main', flexDirection: 'column' }} height='100vh' width='100vw' display="flex" justifyContent="space-evenly" alignItems="center">
+            {showOTPVerification ? (
+                <Box
+                    sx={{
+                        backgroundColor: "yellow.main",
+                        flexDirection: "column",
+                    }}
+                    height="100vh"
+                    width="100vw"
+                    display="flex"
+                    justifyContent="space-evenly"
+                    alignItems="center"
+                >
                     <InsightTitle />
-                    <Stack justifyContent="center" alignItems="center" spacing={2}>
+                    <Stack
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={2}
+                    >
                         <Box width="100%">
-                            <Typography color='black' variant='body1' fontWeight='fontWeightMedium' align="left">Enter verification code sent to {email}</Typography>
+                            <Typography
+                                color="black"
+                                variant="body1"
+                                fontWeight="fontWeightMedium"
+                                align="left"
+                            >
+                                Enter verification code sent to {username}
+                            </Typography>
                         </Box>
-                        <TextFieldYellow onChange={(e) => setOtp(e.target.value)} />
-                        {error ? <Alert severity="error">{error}</Alert> : ''}
+                        <TextFieldYellow
+                            id="otpTextField"
+                            placeholder="Enter OTP"
+                            onChange={(e) =>
+                                setConfirmationCode(e.target.value)
+                            }
+                        />
+                        {error ? <Alert severity="error">{error}</Alert> : ""}
                     </Stack>
-                    <BlackButton onClick={otpApiCall}>Verify</BlackButton>
+                    <BlackButton
+                        onClick={async () => {
+                            await handleSignUpConfirmation();
+                        }}
+                    >
+                        Verify
+                    </BlackButton>
                 </Box>
-                :
-                <Box sx={{ backgroundColor: 'yellow.main', flexDirection: 'column' }} height='100vh' width='100vw' display="flex" justifyContent="space-evenly" alignItems="center">
+            ) : (
+                <Box
+                    sx={{
+                        backgroundColor: "yellow.main",
+                        flexDirection: "column",
+                    }}
+                    height="100vh"
+                    width="100vw"
+                    display="flex"
+                    justifyContent="space-evenly"
+                    alignItems="center"
+                >
                     <InsightTitle />
-                    <Stack justifyContent="center" alignItems="center" spacing={2}>
+                    <Stack
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={2}
+                    >
                         <Box width="100%">
-                            <Typography color='black' variant='body1' fontWeight='fontWeightMedium' align="left">Email</Typography>
+                            <Typography
+                                color="black"
+                                variant="body1"
+                                fontWeight="fontWeightMedium"
+                                align="left"
+                            >
+                                Email
+                            </Typography>
                         </Box>
-                        <TextFieldYellow onChange={(e) => setEmail(e.target.value)} />
+                        <TextFieldYellow
+                            id="usernameField"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        {/* <Box width="100%">
+                            <Typography
+                                color="black"
+                                variant="body1"
+                                fontWeight="fontWeightMedium"
+                                align="left"
+                            >
+                                Username
+                            </Typography>
+                        </Box>
+                        <TextFieldYellow
+                            onChange={(e) => setUsername(e.target.value)}
+                        /> */}
                         <Box width="100%">
-                            <Typography color='black' variant='body1' fontWeight='fontWeightMedium' align="left">Username</Typography>
+                            <Typography
+                                color="black"
+                                variant="body1"
+                                fontWeight="fontWeightMedium"
+                                align="left"
+                            >
+                                Password
+                            </Typography>
                         </Box>
-                        <TextFieldYellow onChange={(e) => setUsername(e.target.value)} />
-                        <Box width="100%">
-                            <Typography color='black' variant='body1' fontWeight='fontWeightMedium' align="left">Password</Typography>
-                        </Box>
-                        <TextFieldYellow onChange={(e) => setPassword(e.target.value)} type="password"/>
-                        {error ? <Alert severity="error">{error}</Alert> : ''}
+                        <TextFieldYellow
+                            onChange={(e) => setPassword(e.target.value)}
+                            id="passwordField"
+                            type="password"
+                        />
+                        {error ? <Alert severity="error">{error}</Alert> : ""}
                     </Stack>
-                    <BlackButton onClick={signupApiCall}>Signup</BlackButton>
+                    <BlackButton
+                        onClick={async () => {
+                            await handleSignUp();
+                        }}
+                    >
+                        Signup
+                    </BlackButton>
                 </Box>
-            }
-
+            )}
         </ThemeProvider>
-
     );
 }
 
