4cf668d3b72f592dcec3f1c4a203681050fc1851
SimJunYou <simjunyou99@gmail.com>
2024-04-12 01:58:08 +0800

    Added Error as a route

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './pages/App';
-import Landing from './pages/Landing'
-import reportWebVitals from './reportWebVitals';
+// AMPLIFY CONFIGURATION
+import { Amplify } from "aws-amplify";
+import amplifyconfig from "./amplifyconfiguration.json";
+
+// Standard imports
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Welcome from './pages/Welcome';
-import History from './pages/History';
 
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./pages/App";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Welcome from "./pages/Welcome";
+import History from "./pages/History";
+import Error from "./pages/Error";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Before anything, make sure Amplify is configured
+Amplify.configure(amplifyconfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="dashboard" element={<App />} />
-        <Route path="welcome" element={<Welcome />} />
-        <Route path="history" element={<History />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Signup />} />
+                <Route path="dashboard" element={<App />} />
+                <Route path="welcome" element={<Welcome />} />
+                <Route path="history" element={<History />} />
+                <Route path="error" element={<Error />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
