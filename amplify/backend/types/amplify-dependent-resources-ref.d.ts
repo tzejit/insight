6309602aaf613d40b11604ba19c9ce2991d762b3
SimJunYou <simjunyou99@gmail.com>
2024-04-12 01:53:53 +0800

    Amplify init changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "insight": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "insight19c1f52d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "userdata": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
