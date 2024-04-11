cae280a9356cd245eec0d49ae7252e2a25bce0af
SimJunYou <simjunyou99@gmail.com>
2024-04-12 01:58:51 +0800

    Added some more necessary props to TextFieldYellow

--- src/components/inputs/TextFieldYellow.js
+++ src/components/inputs/TextFieldYellow.js
@@ -1,20 +1,24 @@
-import { TextField } from '@mui/material';
+import { TextField } from "@mui/material";
 
 function TextFieldYellow(props) {
-    return(
-    <TextField
-    variant="outlined" size="small"
-    onChange={props.onChange}
-    type={props.type}
-    sx={{
-        "& .MuiOutlinedInput-root": {
-            backgroundColor: "yellow.secondary",
-            "& .MuiOutlinedInput-notchedOutline": {
-            borderColor: "yellow.secondary",
-            }
-        }}}/>
-    ) 
-
+    return (
+        <TextField
+            variant="outlined"
+            size="small"
+            onChange={props.onChange}
+            type={props.type}
+            placeholder={props.placeholder}
+            id={props.id}
+            sx={{
+                "& .MuiOutlinedInput-root": {
+                    backgroundColor: "yellow.secondary",
+                    "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "yellow.secondary",
+                    },
+                },
+            }}
+        />
+    );
 }
 
 export default TextFieldYellow;
