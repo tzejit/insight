45e5f3ddc1a78e60616ad9a4abf1ee156033e3ef
Tze Jit Kho <tzejit@gmail.com>
2024-04-14 11:09:12 +0800

    Add poll loading and link to dashboard for report

--- src/components/buttons/YellowButton.js
+++ src/components/buttons/YellowButton.js
@@ -1,11 +1,11 @@
 import { Button } from '@mui/material';
 
 function YellowButton(props) {
-    return(
-    <Button variant="contained" color="secondary" disableElevation sx={{ padding:'0.5em', borderRadius: '1em', width: props.width, margin: '0.5em'}} onClick={props.onClick}> 
-        {props.children}
-    </Button>
-    ) 
+    return (
+        <Button variant="contained" color="secondary" disableElevation sx={{ padding: '0.5em', borderRadius: '1em', width: props.width, margin: '0.5em' }} onClick={props.onClick} disabled={props.disabled}>
+            {props.children}
+        </Button>
+    )
 
 }
 
